7a36d1fdad4b600688ed997b902b0a91650d2f53
ROHIT12-bit <reddyrohit823@gmail.com>
2025-12-09 07:15:42 +0530

    Update config.py

--- config.py
+++ config.py
@@ -2,7 +2,7 @@ API_ID = 26047636
 API_HASH = "d8b1ed69ae1f937c5dd4d3cc8c8de440" 
 BOT_TOKEN = "8400380694:AAEEXOcsF0Cx-nR4KE8kErYxuip-sWKD6cM"
 MONGO_URI = "mongodb+srv://rohitreddyathuru:R6Co7MOjTYQOAqcq@cluster0.xrwjpl9.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
-START_PIC = "https://i.rj1.dev/ADADe.png" 
+START_PIC = "https://i.rj1.dev/aNWlA.png" 
 START_MSG = """<blockquote> Hello Dude!I am Sequence Bot 🤖</blockquote>
 <blockquote>I can help you organize, sequence, and manage your files effortlessly. Send me your files, and I’ll take care of the rest!</blockquote>
 <blockquote>[ᴘᴏᴡᴇʀᴇᴅ ʙʏ ʙᴏᴛsᴋɪɴɢᴅᴏᴍs](https://t.me/BOTSKINGDOMS)</blockquote>"""
@@ -12,6 +12,7 @@ HELP_TXT = """<blockquote><b>🤖 ᴍʏ ɴᴀᴍᴇ: ғɪʟᴇ sᴇǫᴜᴇɴᴄ
 <b>◈ ᴍᴀɴɢᴀ ᴄʜᴀɴɴᴇʟ:</b> <a href="https://t.me/astroMangas"><b>Cʟɪᴄᴋ ʜᴇʀᴇ</b></a><br>
 <b>◈ ᴅᴇᴠᴇʟᴏᴘᴇʀ:</b> <a href="https://t.me/Rioshin"><b>Cʟɪᴄᴋ ʜᴇʀᴇ</b></a>"</blockquote>"""
 OWNER_ID = 8367080346
+
 
 
 
