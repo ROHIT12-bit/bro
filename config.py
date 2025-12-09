713b7659b78e94d840f5066dbaa3540a48a99ca3
ROHIT12-bit <reddyrohit823@gmail.com>
2025-12-09 07:02:48 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,8 +1,26 @@
-API_ID = 20421248 
-API_HASH = "90da70b17365b3709b8a0346a7749ce7" 
-BOT_TOKEN = "0"
-MONGO_URI = "0"
-START_PIC = "https://telegra.ph/file/c39602ab8e3d5932a91d1.jpg" 
-START_MSG = "<b>Hi There...! \n\nI am a File Sequence Bot.\nI can sequence your files effortlessly.\n\nMy Owner: @Its_Sahil_Ansari</b>"
-HELP_TXT = "<b><i>About Us..\n\n‣ Made for : Anime x Hunters\n‣ Owner : @Its_Sahil_Ansari\n‣ Worked For : @Anime_x_hunters</i></b>"
-OWNER_ID = 5090651635
+API_ID = 26047636 
+API_HASH = "d8b1ed69ae1f937c5dd4d3cc8c8de440" 
+BOT_TOKEN = "8400380694:AAEEXOcsF0Cx-nR4KE8kErYxuip-sWKD6cM"
+MONGO_URI = "mongodb+srv://rohitreddyathuru:R6Co7MOjTYQOAqcq@cluster0.xrwjpl9.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
+START_PIC = "https://i.rj1.dev/ADADe.png" 
+START_MSG = """<blockquote> Hello Dude!I am Sequence Bot 🤖</blockquote>
+<blockquote>I can help you organize, sequence, and manage your files effortlessly. Send me your files, and I’ll take care of the rest!</blockquote>
+<blockquote>[ᴘᴏᴡᴇʀᴇᴅ ʙʏ ʙᴏᴛsᴋɪɴɢᴅᴏᴍs](https://t.me/BOTSKINGDOMS)</blockquote>"""
+HELP_TXT = """<blockquote><b>🤖 ᴍʏ ɴᴀᴍᴇ: ғɪʟᴇ sᴇǫᴜᴇɴᴄᴇ ʙᴏᴛ</b>
+<b>◈ ᴏᴡɴᴇʀ:</b> <a href="https://t.me/Rioshin"><b>Cʟɪᴄᴋ ʜᴇʀᴇ</b></a><br>
+<b>◈ ʙᴏᴛs ᴄʜᴀɴɴᴇʟ:</b> <a href="https://t.me/botskingdoms"><b>Cʟɪᴄᴋ ʜᴇʀᴇ</b></a><br>
+<b>◈ ᴍᴀɴɢᴀ ᴄʜᴀɴɴᴇʟ:</b> <a href="https://t.me/astroMangas"><b>Cʟɪᴄᴋ ʜᴇʀᴇ</b></a><br>
+<b>◈ ᴅᴇᴠᴇʟᴏᴘᴇʀ:</b> <a href="https://t.me/Rioshin"><b>Cʟɪᴄᴋ ʜᴇʀᴇ</b></a>"</blockquote>"""
+OWNER_ID = 8367080346
+
+
+
+
+
+
+
+
+
+
+
+
